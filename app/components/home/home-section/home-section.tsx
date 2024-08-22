c756cc34964981868e7d3f2c7cca647885507ef7
muhiddin0 <kabraliev2005@gmail.com>
2024-08-21 19:47:31 -0700

    main

--- app/components/home/home-section/home-section.tsx
+++ app/components/home/home-section/home-section.tsx
@@ -4,17 +4,17 @@ import React from "react";
 type Props = {};
 
 export const HomeSection: React.FC<Props> = (props) => {
-  return (
-    <section className="text-white">
-      <div className="container relative z-[10] flex items-center min-h-[1084px] flex-col !pt-6">
-        <h1 className="text-[30px] sm:text-[30px] md:text-[60px] xl:text-[80px] font-bold capitalize max-w-[861px] text-center">
-          Make your interior more minimalistic & modern
-        </h1>
-        <p className="max-w-[400px] text-center">
-          Turn your room with panto into a lot more minimalist and modern with
-          ease and speed
-        </p>
-      </div>
-    </section>
-  );
+    return (
+        <section className="text-white">
+            <div className="container relative z-[10] flex items-center justify-center min-h-screen flex-col !pt-6">
+                <h1 className="text-[30px] sm:text-[30px] md:text-[60px] xl:text-[80px] font-bold capitalize max-w-[861px] text-center">
+                    Make your interior more minimalistic & modern
+                </h1>
+                <p className="max-w-[400px] text-center">
+                    Turn your room with panto into a lot more minimalist and
+                    modern with ease and speed
+                </p>
+            </div>
+        </section>
+    );
 };
